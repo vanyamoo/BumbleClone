b5f9f963c4f259430f38a9e3ea9914fd960270ed
vanya <vanya.moo@gmail.com>
2024-10-14 15:26:36 +0100

    Created FilteView

---
+++ BumbleClone/Components/FilterView.swift
@@ -0,0 +1,18 @@
+//
+//  FilterView.swift
+//  BumbleClone
+//
+//  Created by Vanya Mutafchieva on 14/10/2024.
+//
+
+import SwiftUI
+
+struct FilterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FilterView()
+}
