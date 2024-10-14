3a7ab95b87fbd0a7bce03652232235b5e9015bf9
vanya <vanya.moo@gmail.com>
2024-10-14 13:15:55 +0100

    Initial Commit

---
+++ BumbleClone/BumbleCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  BumbleCloneApp.swift
+//  BumbleClone
+//
+//  Created by Vanya Mutafchieva on 14/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct BumbleCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
