ea797cf7ff8ffb8e8debeb4bbb54bd2a0ded6699
vanya <vanya.moo@gmail.com>
2024-10-15 17:11:59 +0100

    Added Swipe logic to a dummy Rectangle view in Home screen

--- BumbleClone/Core/HomeView.swift
+++ BumbleClone/Core/HomeView.swift
@@ -6,11 +6,15 @@
 //
 
 import SwiftUI
+import SwiftfulUI
 
 struct HomeView: View {
     
     @State private var filters: [String] = ["Everyone", "Trending"]
     @AppStorage("bumble_home_filter") private var selectedFilter = "Everyone"
+    @State private var allUsers: [User] = []
+    @State private var selectedIndex: Int = 0
+    @State private var cardOffsets: [Int:Bool] = [:] // UserId : (Direction is right == TRUE)
     
     var body: some View {
         ZStack {
@@ -23,11 +27,42 @@ struct HomeView: View {
                     .background(
                         Divider(), alignment: .bottom)
                 
-                CardView()
-                    .padding()
+//                CardView()
+                ZStack {
+                    if !allUsers.isEmpty {
+                        ForEach(Array(allUsers.enumerated()), id: \.offset) { (index, user) in
+                            
+                            let isPrevious = (selectedIndex - 1 == index)
+                            let isCurrent = (selectedIndex == index)
+                            let isNext = (selectedIndex + 1 == index)
+                            if isPrevious || isCurrent || isNext {
+                                let offsetValue = cardOffsets[user.id]
+                                
+                                userProfileCell(index: index)
+                                    .zIndex(Double(allUsers.count - index)) // stacks teh cards in reverse order
+                                    .offset(x: offsetValue == nil ? 0 : offsetValue == true ? 900 : -900)
+                            }
+                        }
+                    } else {
+                        ProgressView()
+                    }
+                }
+                .frame(maxHeight: .infinity)
+                .animation(.smooth, value: cardOffsets)
             }
-            //.padding()
+            .padding()
         }
+        .task {
+            await getData()
+        }
+        .toolbarVisibility(.hidden, for: .navigationBar)
+    }
+    
+    private func userDidSelect(index: Int, isLike: Bool) {
+        let user = allUsers[index]
+        cardOffsets[user.id] = isLike
+        
+        selectedIndex += 1
     }
     
     private var header: some View {
@@ -42,6 +77,29 @@ struct HomeView: View {
         .font(.title2)
         .fontWeight(.medium)
         .foregroundStyle(.bumbleBlack)
+    }
+    
+    private func userProfileCell(index: Int) -> some View {
+        Rectangle()
+            .fill(index == 0 ? .red : .blue)
+            .overlay(Text("\(index)"))
+            .withDragGesture(
+                .horizontal,
+//                                        minimumDistance: <#T##CGFloat#>,
+                resets: true,
+                rotationMultiplier: 1.05,
+//                                        scaleMultiplier: 0.9,
+                onChanged: { dragOffset in
+                    //offset = dragOffset.width
+                },
+                onEnded: { dragOffset in
+                    if dragOffset.width < -50 {
+                        userDidSelect(index: index, isLike: false)
+                    } else if dragOffset.width > 50 {
+                        userDidSelect(index: index, isLike: true)
+                    }
+                }
+            )
     }
     
     private var headerLeft: some View {
@@ -75,9 +133,15 @@ struct HomeView: View {
                 //
             }
     }
-    private var filterSection: some View {
-        Circle()
-    }
+    
+    private func getData() async {
+        guard allUsers.isEmpty else { return }
+            do {
+                allUsers = try await DatabaseHelper().getUsers()
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
 }
 
 #Preview {
